d06dcebdff40510091487107ea94eadb3d6ad719
ASbeletsky <antonbeletsky@gmail.com>
2015-10-10 01:06:14 +0300

    Create three layered architecture solution

---
+++ WebChat/src/WebChat.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebChat.WebUI.Startup))]
+namespace WebChat.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
